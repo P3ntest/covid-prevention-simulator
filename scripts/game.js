5c5a1803d0adbb84737f21a8dd957d34dba4d439
P3ntest <juliusvanvoorden@outlook.com>
2020-10-02 14:58:57 +0200

    add default pixi strucutre

---
+++ scripts/game.js
@@ -0,0 +1,16 @@
+//Aliases
+let Application = PIXI.Application,
+    loader = PIXI.loader,
+    resources = PIXI.loader.resources,
+    Sprite = PIXI.Sprite;
+
+//Create a Pixi Application
+let app = new Application({});
+
+//Add the canvas that Pixi automatically created for you to the HTML document
+document.body.appendChild(app.view);
+
+app.renderer.view.style.position = "absolute";
+app.renderer.view.style.display = "block";
+app.renderer.autoResize = true;
+app.renderer.resize(window.innerWidth, window.innerHeight);
