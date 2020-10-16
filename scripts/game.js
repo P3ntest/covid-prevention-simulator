45714692d11477ca362feb867d8244e1f33d9ca8
P3ntest <xXraxFraeyXx@gmail.com>
2020-10-16 09:39:56 +0200

    add mouse events

--- scripts/game.js
+++ scripts/game.js
@@ -2,7 +2,9 @@
 let Application = PIXI.Application,
     loader = PIXI.loader,
     resources = PIXI.loader.resources,
-    Sprite = PIXI.Sprite;
+    Sprite = PIXI.Sprite,
+    TextureCache = PIXI.utils.TextureCache,
+    Container = PIXI.Container;
 
 //Create a Pixi Application
 let app = new Application({});
@@ -14,3 +16,27 @@ app.renderer.view.style.position = "absolute";
 app.renderer.view.style.display = "block";
 app.renderer.autoResize = true;
 app.renderer.resize(window.innerWidth, window.innerHeight);
+
+let mapContainer = new Container();
+app.stage.addChild(mapContainer);
+//mapContainerMouseDown.interactive = true;
+let mapContainerMouseDown = false;
+
+mapContainer.on("mousedown", () => {
+    mapContainerMouseDown = true;
+    console.log(true);
+});
+mapContainer.on("mouseup", () => {
+    mapContainerMouseDown = false;
+});
+mapContainer.on("pointermove", ((event) => {
+    if (mapContainerMouseDown) {
+        console.log(event);
+    }
+}));
+
+PIXI.loader.add("images/cat.png").load(() => {
+    let catSprite = new Sprite(TextureCache["images/cat.png"]);
+
+    mapContainer.addChild(catSprite);
+});
