319f34a77f00437fe06b1451efe6158ea702647d
Joshi-Kartoshi <52852177+Joshi-Kartoshi@users.noreply.github.com>
2020-11-27 11:23:13 +0100

    update hotspots

--- scripts/hotspots.js
+++ scripts/hotspots.js
@@ -2,25 +2,35 @@ let hotspots = [
     {
         x: 0.53,
         y: 0.19,
-        name: "Moscow"
+        name: "Moscow",
+        happiness: 0.8
     },
     {
         x: 0.48,
         y: 0.24,
-        name: "Europe"
+        name: "Europe",
     },
     {
         x: 0.7,
         y: 0.17,
-        name: "Siberia"
+        name: "Siberia",
+        hapiness: 0.4
+    },
+    {
+        x:0.18,
+        y:0.2,
+        name: "Canada",
+        hapiness: 0.9
     }
 ];
 
 
 //Set defualt values for all hotspots
 hotspots.forEach(hotspot => {
-    hotspot.happiness = 1;
-    hotspot.trust = 1;
+    if (!hotspot.happiness)
+        hotspot.happiness = 1;
+    if (!hotspot.trust)
+        hotspot.trust = 1;
     hotspot.infections = 0;
     hotspot.deaths = 0;
 })
